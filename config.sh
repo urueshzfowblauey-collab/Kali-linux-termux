738b8d027a24ded0f692fb1757e25f84e87dccc2
urueshzfowblauey-collab <urueshzfowblauey@gmail.com>
2026-06-07 05:02:40 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -16,3 +16,22 @@ TOOLS["wfuzz"]=""
 TOOLS["netcat-openbsd"]=""
 TOOLS["curl"]=""
 TOOLS["wget"]=""
+
+declare -A TOOL_BIN
+TOOL_BIN["nmap"]="nmap"
+TOOL_BIN["sqlmap"]="sqlmap"
+TOOL_BIN["hydra"]="hydra"
+TOOL_BIN["nikto"]="nikto"
+TOOL_BIN["john"]="john"
+TOOL_BIN["aircrack-ng"]="aircrack-ng"
+TOOL_BIN["hashcat"]="hashcat"
+TOOL_BIN["metasploit-framework"]="msfconsole"
+TOOL_BIN["beef-xss"]="beef"
+TOOL_BIN["gobuster"]="gobuster"
+TOOL_BIN["dirb"]="dirb"
+TOOL_BIN["wfuzz"]="wfuzz"
+TOOL_BIN["netcat-openbsd"]="nc"
+TOOL_BIN["curl"]="curl"
+TOOL_BIN["wget"]="wget"
+
+TOOL_ORDER=("nmap" "sqlmap" "hydra" "nikto" "john" "aircrack-ng" "hashcat" "metasploit-framework" "beef-xss" "gobuster" "dirb" "wfuzz" "netcat-openbsd" "curl" "wget")
